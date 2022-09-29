2890d8588d55dd8f0a51160e41588297b823c142
chasechri <christensonchase451@gmail.com>
2022-09-29 18:30:51 -0500

    homepage fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 // import logo from "./logo.svg";
-import React from 'react'
-import './App.css'
-import Header from './components/Header'
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Homepage from './pages/Homepage'
+import Homepage from "./pages/Homepage";
 
 function App() {
-	return (
-		<div>
-			<Header />
-			<Homepage><Homepage/>
+  return (
+    <div>
+      <Header />
+      <Homepage></Homepage>
       <Footer></Footer>
-		</div>
-	)
+    </div>
+  );
 }
 
-export default App
+export default App;
