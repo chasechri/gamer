7087e43675c5396d05e08f79fd286dd79d8e8cc7
markg <mark.grangaard@gmail.com>
2022-09-29 20:28:08 -0500

    added tailwind (maybe other changes but app still works)

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,15 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
-import Footer from "./components/Footer";
+import Footer from './components/Footer'
 import Homepage from './pages/Homepage'
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<Homepage><Homepage/>
-      <Footer></Footer>
+			<Homepage />
+			<Footer />
 		</div>
 	)
 }
