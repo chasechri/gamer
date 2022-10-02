38cfa9e46e6dfa05b44642f7c2cfbb1f56fa76fb
Kilimba <dyaul003@umn.edu>
2022-10-02 15:18:39 -0500

    react router now works but the profile page is blank

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 // import logo from "./logo.svg";
 import React from 'react';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-
-
-
-  
+} from '@apollo/client';  
 
 import './index.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import NoMatch from './pages/NoMatch';
 
 const httpLink = createHttpLink({
 	uri: '/graphql',
@@ -27,18 +30,41 @@ const httpLink = createHttpLink({
   });
 
 function App() {
-	return (
-		<ApolloProvider client={client}>
-			
-		<div>
-			<Header />
-			<div>
-			<Homepage />
-			</div>
-			<Footer />
-		</div>
-		</ApolloProvider>
- );
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="">
+          <Header />
+          <div className="">
+            <Routes>
+              <Route
+                path="/"
+                element={<Homepage />}
+              />
+              <Route
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/signup"
+                element={<Signup />}
+              />
+              <Route
+                path="/profile"
+                element={<Profile />}
+              />
+			   
+				<Route
+				 path="*"
+				 element={<NoMatch />}
+					/>
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
