8b872ae28f348f9e78bf255ec925ea1385404276
markg <mark.grangaard@gmail.com>
2022-10-04 13:46:15 -0500

    posts render on load and are filterable (individually)

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,163 +1,120 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import UserCard from '../components/UserCard';
-import UserList from '../components/UserList'
-
+import Filters from '../components/Filters';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_POSTS } from '../utils/queries';
 
 const Homepage = () => {
-	// using the query hook to make a request 
+	// using the query hook to make a request
 	const { loading, data } = useQuery(QUERY_POSTS);
 	// checking if there's data to post (if not then store it in empty array)
-	const originalPlayers = data?.posts || [];
-	console.log(originalPlayers);
-  
-  // will later make call to server to grab all players React.useEffect to call server to grab all players
-	// const originalPlayers = [
-	// 	{
-	// 		username: 'Gamertag',
-	// 		rank: 'Casual',
-	// 		platform: 'PC',
-	// 		hours: 'AM',
-	// 		comms: 'Y',
-	// 	},
-	// 	{
-	// 		username: 'Gamertag',
-	// 		rank: 'Gold',
-	// 		platform: 'PS5',
-	// 		hours: 'PM',
-	// 		comms: 'Y',
-	// 	},
-	// ]
-	
+	const initialPosts = data?.posts || [];
+
+	// will later make call to server to grab all players React.useEffect to call server to grab all players
 
 	const [isChecked, setIsChecked] = useState(false);
-
-
-
-	const [players, setPlayers] = useState(originalPlayers)
-
-	function handleFilterClick(c) {
-
-	}
-	
-	function clearFilters(c) {
-		setIsChecked(isChecked)
-		console.log('setting to isChecked')
-		if (!isChecked) {
-			console.log('isChecked path')
-			
-			setPlayers(originalPlayers);
-		} else {
-			setIsChecked(isChecked);
-			setPlayers(originalPlayers)
-			console.log('nothing checked')
+	const [posts, setPosts] = useState(initialPosts);
+	useEffect(() => {
+		if (loading === false && posts) {
+			setPosts(initialPosts);
 		}
+	}, [loading, data]);
 
-	}
+	function handleFilterClick(c) {}
 
+	// function clearFilters(c) {
+	// 	setIsChecked(isChecked);
+	// 	console.log('setting to isChecked');
+	// 	if (!isChecked) {
+	// 		console.log('isChecked path');
+
+	// 		setPosts(data?.posts);
+	// 	} else {
+	// 		setIsChecked(isChecked);
+	// 		setPosts(data?.posts);
+	// 		console.log('nothing checked');
+	// 	}
+	// }
 
 	function handleRank(rank) {
 		console.log('this is rank', rank.target.value);
-		setIsChecked(!isChecked)
-		if (!isChecked)
-			{
-				console.log('isChecked = true working')
-				setPlayers(players.filter((player) => player.rank === rank.target.value))
-				
-			}
+		setIsChecked(!isChecked);
+		if (!isChecked) {
+			console.log(rank.target.value);
+			setPosts(posts.filter((post) => post.rank === rank.target.value));
+			console.log(posts);
+		}
 		if (isChecked) {
-			setPlayers(originalPlayers)
-			console.log('isChecked = false working')
-
-			
+			setPosts(initialPosts);
+			console.log('isChecked = false working');
 		}
 	}
-	
+
 	function handlePlatform(platform) {
-		setIsChecked(!isChecked)
-		if (!isChecked)
-			{
-				console.log('isChecked = true working')
-				setPlayers(players.filter((player) => player.platform === platform.target.value))
-				
-			}
+		setIsChecked(!isChecked);
+		if (!isChecked) {
+			console.log('isChecked = true working');
+			setPosts(posts.filter((post) => post.platform === platform.target.value));
+		}
 		if (isChecked) {
-			setPlayers(originalPlayers)
-			console.log('isChecked = false working')
-
-			
-		}	
-	}
-	
-	function handleHours(hours) {
-		setIsChecked(!isChecked)
-		if (!isChecked)
-			{
-				console.log('isChecked = true working')
-				setPlayers(players.filter((player) => player.hours === hours.target.value))
-				
-			}
-		if (isChecked) {
-			setPlayers(originalPlayers)
-			console.log('isChecked = false working')
-
-			
+			setPosts(initialPosts);
+			console.log('isChecked = false working');
 		}
 	}
-	
-	function handleComms(comms) {
-		setIsChecked(!isChecked)
-		if (!isChecked)
-			{
-				console.log('isChecked = true working')
-				setPlayers(players.filter((player) => player.comms === comms.target.value))
-				
-			}
-		if (isChecked) {
-			setPlayers(originalPlayers)
-			console.log('isChecked = false working')
 
-			
+	function handleHours(hours) {
+		setIsChecked(!isChecked);
+		if (!isChecked) {
+			console.log('isChecked = true working');
+			setPosts(posts.filter((post) => post.hours === hours.target.value));
+		}
+		if (isChecked) {
+			setPosts(initialPosts);
+			console.log('isChecked = false working');
+		}
+	}
+
+	function handleComms(comms) {
+		setIsChecked(!isChecked);
+		if (!isChecked) {
+			console.log('isChecked = true working');
+			setPosts(posts.filter((post) => post.comms === comms.target.value));
+		}
+		if (isChecked) {
+			setPosts(initialPosts);
+			console.log('isChecked = false working');
 		}
 	}
 	// const { loading, data } = useQuery(QUERY_THOUGHTS)
 	// const { data: userData } = useQuery(QUERY_ME_BASIC)
 	// const users = data?.users || []
 
-
-
 	return (
 		<main>
-
-			<div
-				name='user-list'
-				className='flex flex-row'
-			>
-				<UserList
-				title='Player List'
-					players={players}
+			<div name='homepage' className='flex flex-row justify-around'>
+				<Filters
+					title='Filter List'
 					handleRank={handleRank}
 					handleHours={handleHours}
 					handlePlatform={handlePlatform}
 					handleComms={handleComms}
-					clearFilters={clearFilters} 
+					// clearFilters={clearFilters}
 					isChecked={isChecked}
 					setIsChecked={setIsChecked}
-
 				/>
-
 			</div>
-			
+
 			<div>
-			{loading ? (
-        	<div>Loading...</div>
+				{loading ? (
+					<div>Loading...</div>
 				) : (
-					<UserCard players={players} title="Don't be discouraged to make the first post" />
+					<UserCard
+						posts={posts}
+						title="Don't be discouraged to make the first post"
+					/>
 				)}
 			</div>
-		
 		</main>
 	);
 };
