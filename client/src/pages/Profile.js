8447b876ea7de190849db2abbc6dbfda12f509fa
Kilimba <dyaul003@umn.edu>
2022-10-04 23:44:11 -0500

    got some of the data to show up

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,92 +1,49 @@
 import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
-import UserForm from '../components/UserForm';
-import UserCard from '../components/UserCard';
-import FriendList from '../components/FriendList';
 
-import { useQuery, useMutation } from '@apollo/client';
-import { QUERY_USER, QUERY_ME,  } from '../utils/queries';
-import { ADD_FRIEND } from '../utils/mutations';
-import Auth from '../utils/auth';
 
-const Profile = (props) => {
-  const { username: userParam } = useParams();
+import { useQuery } from '@apollo/client';
+import { QUERY_ME,  } from '../utils/queries';
 
-  const [addFriend] = useMutation(ADD_FRIEND);
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam },
+
+
+const Profile = () => {
+  const { username } = useParams();
+
+  const { data } = useQuery(QUERY_ME, {
+    variables: { username: username },
   });
   
-  const user = data?.me || data?.user || {};
+  const user = data?.me ||  {};
 
   console.log(data)
    
 
-  // navigate to personal profile page if username is yours
-  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Navigate to="/profile:username" />;
-  }
+  
 
   
-  if (loading) {
-    return <div>Loading...</div>;
-  }
   
   if (!user?.username) {
     return (
       <h4>
-        You need to be logged in to see this. Use the navigation links above to
-        sign up or log in! 
+        NO USER EXISTS
       </h4>
       
     );
   }
 
-  const handleClick = async () => {
-    try {
-      await addFriend({
-        variables: { id: user._id },
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  };
+  
   
 
   return (
     <div>
-      <div className="flex-row mb-3">
-        <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-        </h2>
-        
-
-        {userParam && (
-          <button className="btn ml-auto" onClick={handleClick}>
-            Add Friend
-          </button>
-        )}
-      </div>
-
-      <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">
-          <UserCard
-            posts={user.posts}
-            title={`${user.username}'s thoughts...`}
-          />
-        </div>
-
-        <div className="col-12 col-lg-3 mb-3">
-          <FriendList
-            username={user.username}
-            friendCount={user.friendCount}
-            friends={user.friends}
-          />
-        </div>
-      </div>
-      <div className="mb-3">{!userParam && <UserForm />}</div>
-    </div>
+    <br></br>
+    <h2 >Viewing {`${user.username}'s`} profile</h2>
+    <p>{`${user.email}'s`}</p>
+    <p>xbox, ps4, ps5</p>
+    
+  </div>
   );
 };
 
