814f44427b08cab45b92b3fb73e65abea0d563b7
markg <mark.grangaard@gmail.com>
2022-10-05 21:33:57 -0500

    cleaned up styling

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,40 +5,58 @@ import { faHomeLg } from '@fortawesome/free-solid-svg-icons';
 import Auth from '../../utils/auth';
 
 const Header = () => {
+	const logout = (event) => {
+		event.preventDefault();
+		Auth.logout();
+	};
+	return (
+		<header>
+			<div className='flex flex-row px-4'>
+				<Link to='/'>
+					<h1>
+						{' '}
+						Gamer
+						<span>
+							{' '}
+							<FontAwesomeIcon icon={faHomeLg}></FontAwesomeIcon>{' '}
+						</span>
+					</h1>
+				</Link>
+				<Link className='mx-4' to='/news'>
+					<h1 className='px-2'>News</h1>
+				</Link>
+				<nav>
+					{Auth.loggedIn() ? (
+						<>
+							<div className='px-2 flex flex-row content-end'>
+								<div>
+									<Link to='/profile'>Profile</Link>
+								</div>
 
-  const logout = (event) => {
-    event.preventDefault();
-    Auth.logout();
-  };
-  return (
-    <header>
-      <div>
-      <Link to="/">
-        <h1> Gamer
-       <span> <FontAwesomeIcon icon={faHomeLg} ></FontAwesomeIcon> </span>
-        </h1>
-        </Link>
-        <Link to="/news">News</Link>
-        <nav>
-          {Auth.loggedIn() ? (
-            <>
-              <Link to="/profile">Profile</Link>
-              
-              <a href="/" onClick={logout}>
-                Logout
-              </a>
-            </>
-          ) : (
-            <>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
-            </>
-          )}
-        </nav>
-      </div>
-    </header>
-  );
+								<a
+									className='px-2 flex flex-row content-end'
+									href='/'
+									onClick={logout}
+								>
+									Logout
+								</a>
+							</div>
+						</>
+					) : (
+						<>
+							<div className='px-2 flex flex-row content-end'>
+								<Link className='mr-4' to='/login'>
+									Login
+								</Link>
 
+								<Link to='/signup'>Signup</Link>
+							</div>
+						</>
+					)}
+				</nav>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,29 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import UserCard from '../components/UserCard';
-
 
 import Filters from '../components/Filters';
 
+import { useQuery } from '@apollo/client';
 
-
-
-
-
-import { useQuery} from '@apollo/client';
-
-import { QUERY_POSTS} from '../utils/queries';
-
+import { QUERY_POSTS } from '../utils/queries';
 
 const Homepage = () => {
-	// using the query hook to make a request 
+	// using the query hook to make a request
 	const { loading, data } = useQuery(QUERY_POSTS);
-
-	
-	
-	
-
-
-
+	useEffect(() => {
+		if (loading === false && posts) {
+			setPosts(originalPosts);
+		}
+	}, [loading, data]);
 
 	// checking if there's data to post (if not then store it in empty array)
 	const originalPosts = data?.posts || [];
@@ -31,79 +22,98 @@ const Homepage = () => {
 
 	const [isChecked, setIsChecked] = useState(false);
 
+	const [posts, setPosts] = useState(originalPosts);
 
-
-	const [posts, setPosts] = useState(originalPosts)
-
-	
 	function clearFilters(c) {
-		setIsChecked(isChecked)
-		console.log('setting to isChecked')
+		setIsChecked(isChecked);
+		console.log('setting to isChecked');
 		if (!isChecked) {
-			console.log('isChecked path')
-			
+			console.log('isChecked path');
+
 			setPosts(originalPosts);
 		} else {
 			setIsChecked(isChecked);
-			setPosts(originalPosts)
-			console.log('nothing checked')
+			setPosts(originalPosts);
+			console.log('nothing checked');
 		}
-
 	}
-
 
 	function handleRank(rank) {
 		console.log('this is rank', rank.target.value);
-		setIsChecked(!isChecked)
-		if (!isChecked)
-			{
-				console.log('isChecked = true working')
-				setPosts(posts.filter((post) => post.rank === rank.target.value))
-				
-			}
+		setIsChecked(!isChecked);
+		if (!isChecked) {
+			console.log('isChecked = true working');
+			setPosts(posts.filter((post) => post.rank === rank.target.value));
+		}
 		if (isChecked) {
-			setPosts(originalPosts)
-			console.log('isChecked = false working')
-
-			
+			setPosts(originalPosts);
+			console.log('isChecked = false working');
 		}
 	}
 
-	
+	function handleHours(hours) {
+		setIsChecked(!isChecked);
+		if (!isChecked) {
+			console.log('isChecked = true working');
+			setPosts(posts.filter((post) => post.hours === hours.target.value));
+		}
+		if (isChecked) {
+			setPosts(originalPosts);
+			console.log('isChecked = false working');
+		}
+	}
 
+	function handleComms(comms) {
+		setIsChecked(!isChecked);
+		if (!isChecked) {
+			console.log('isChecked = true working');
+			setPosts(posts.filter((post) => post.comms === comms.target.value));
+		}
+		if (isChecked) {
+			setPosts(originalPosts);
+			console.log('isChecked = false working');
+		}
+	}
+
+	function handlePlatform(platform) {
+		setIsChecked(!isChecked);
+		if (!isChecked) {
+			console.log('isChecked = true working');
+			setPosts(posts.filter((post) => post.platform === platform.target.value));
+		}
+		if (isChecked) {
+			setPosts(originalPosts);
+			console.log('isChecked = false working');
+		}
+	}
 
 	return (
-
-		<main name='homepage' className='flex-col py-20'>
+		<main name='homepage' className='flex-col py-10'>
 			<div className='flex-row'>
-
 				<Filters
-				title='Player List'
+					title='Player List'
 					posts={posts}
 					handleRank={handleRank}
-					clearFilters={clearFilters} 
+					handleHours={handleHours}
+					handleComms={handleComms}
+					handlePlatform={handlePlatform}
+					clearFilters={clearFilters}
 					isChecked={isChecked}
 					setIsChecked={setIsChecked}
-
 				/>
 			</div>
 
 			<div className='flex flex-row justify-around container '>
 				{loading ? (
 					<div>Loading...</div>
-
 				) : (
-					
-					<UserCard posts={posts} title="Don't be discouraged to make the first post" />
+					<UserCard
+						posts={posts}
+						title="Don't be discouraged to make the first post"
+					/>
 				)}
 			</div>
-			
-          
-        
-		
 		</main>
-	
-
 	);
 };
 
